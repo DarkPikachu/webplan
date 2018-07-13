cadb7bb4c011959191d417fd4d13bb26207fc4a1
Dark Pikachu <Dark_Knightt@hotmail.com>
2018-07-13 15:32:18 +0700

    add font awesome
    add font awesome

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <script>windows.laravel = { csrfToken : '{{ csrf_token() }}'</script>
+        <!--script>windows.laravel = { csrfToken : '{{ csrf_token() }}'};</script-->
         
         <title>กองนโยบายและแผน</title>
 
